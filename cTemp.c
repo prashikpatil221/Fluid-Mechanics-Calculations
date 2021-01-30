dd25585ad0ce511849e7658b394c42a10577e6a2
Prashik Patil <70844686+prashikpatil221@users.noreply.github.com>
2021-01-30 18:40:50 +0530

    Create cTemp.c

---
+++ cTemp.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<math.h>
+#include <ctype.h> 
+#include<string.h>
+
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+
+		
+
+	return 0;
+}	
