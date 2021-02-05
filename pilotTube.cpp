42cc7e95df6d40da8db2566f4b1383eabeaa07dc
Prashik Patil <70844686+prashikpatil221@users.noreply.github.com>
2021-02-05 19:54:35 +0530

    created

---
+++ pilotTube.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+	#ifndef ONLINE_JUDGE
+		freopen("input.txt","r",stdin);
+		freopen("output.txt","w",stdout);
+	#endif
+	float h1,h2,dif;
+	float g=9.81;
+	cout<<"input H1 in cm : ";
+	cin>>h1;
+	cout<<endl;
+	cout<<"input H2 in cm : ";
+	cin>>h2;
+	cout<<endl;
+	dif=abs(h1-h2);
+	cout<<"ΔH = "<<dif<<endl;
+	float x= 2*g*dif;
+	float v = pow(x,0.5);
+
+	cout<<"V = "<<v<<endl;
+	
+	return 0;
+}
